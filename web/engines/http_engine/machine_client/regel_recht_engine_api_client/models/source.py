b02e8fa83133ca77cbc4e75baa6008c9842cd15a
Anne Schuth <anne.schuth@rijksoverheid.nl>
2025-12-18 09:18:41 +0100

    fix: handle missing BELASTINGDIENST box data in template (#312)
    Add get() method to Source class to enable safe dict-like access with
    defaults. Update dashboard template to use get('box1', [{}]) pattern
    to prevent UndefinedError when tax box data is missing for a profile.
    
    Fixes 500 error when accessing /?bsn=200000003.

--- web/engines/http_engine/machine_client/regel_recht_engine_api_client/models/source.py
+++ web/engines/http_engine/machine_client/regel_recht_engine_api_client/models/source.py
@@ -1,5 +1,5 @@
 from collections.abc import Mapping
-from typing import TYPE_CHECKING, Any, TypeVar
+from typing import TYPE_CHECKING, Any, TypeVar, Union
 
 from attrs import define as _attrs_define
 from attrs import field as _attrs_field
@@ -63,3 +63,6 @@ class Source:
 
     def __contains__(self, key: str) -> bool:
         return key in self.additional_properties
+
+    def get(self, key: str, default: Any = None) -> Union[list["SourceAdditionalPropertyItem"], Any]:
+        return self.additional_properties.get(key, default)
